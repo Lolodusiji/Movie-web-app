2a3ee1a890e911b3337abc59c1686396ba4bbe2d
Lolodusiji <sholaodusiji@gmail.com>
2023-09-14 23:12:51 +0100

    fixes

--- movie web app/src/Pages/Dashboard/Dashboard.jsx
+++ movie web app/src/Pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import "./Dashboard.css";
 import Sidebar  from '../LandingPage/Component/Sidebar';
+import Trailer from "/imgs/trailer.svg";
 
 const Dashboard = () => {
   return (
     <div>
+        <div className="general">
         <Sidebar/>
+            <div className="trailer">
+                <img id="trailer" src={Trailer} alt=""/>
+            </div>
+        </div>
     </div>
   )
 }
